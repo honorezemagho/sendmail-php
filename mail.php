6de6ce79b91bc7ddd1685cbf0a83c57d75df3920
honorezemagho <honorezemagho@gmail.com>
2020-09-09 14:25:30 +0000

    add sedmail email sample send

---
+++ mail.php
@@ -0,0 +1,25 @@
+<?php
+// Uncomment the next line if you're using a dependency loader (such as Composer) (recommended)
+require 'vendor/autoload.php';
+
+// Uncomment the next line if you're not using a dependency loader (such as Composer), replacing <PATH TO> with the path to the sendgrid-php.php file
+require_once './sendgrid-php.php';
+
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom("perfume@technt.org", "Example User");
+$email->setSubject("Sending with Twilio SendGrid is Fun");
+$email->addTo("zankafred@gmail.com", "Example User");
+$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->addContent(
+    "text/html",
+    "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+$sendgrid = new \SendGrid('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');
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: ' . $e->getMessage() . "\n";
+}
